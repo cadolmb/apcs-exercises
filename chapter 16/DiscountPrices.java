ec992ef38644daee478ffb596eeb8dde1db360b7
Matthew Cadol <link0833@gmail.com>
2019-09-09 12:46:58 -0600

    Ch. 16 Discount Prices

---
+++ chapter 16/DiscountPrices.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DiscountPrices {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter price in cents: ");
+
+    int price = s.nextInt();
+    if (price > 1000) {
+      price *= 0.9;
+    }
+    System.out.println("Dicounted price: " + price + " cents.");
+  }
+
+}

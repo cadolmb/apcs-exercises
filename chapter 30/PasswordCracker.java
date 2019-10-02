989ff1bc9943043dc3e39c7096d812f9b1c70484
Matthew Cadol <cadolmb@s.dcsdk12.org>
2019-10-01 22:48:52 -0600

    attempting converting numbers to string

--- chapter 30/PasswordCracker.java
+++ chapter 30/PasswordCracker.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PasswordCracker {
 
@@ -12,9 +13,9 @@ public class PasswordCracker {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(
-            "Enter a password that is" + maxPassLength + " or less characters long" +
+            "Enter a password that is " + maxPassLength + " or less characters long" +
             //"\nand contains only letters and numbers"
-            "\nand contains only lowercase letters"
+            "\nand contains only lowercase letters:\n"
         );
 
         password = scanner.nextLine();
@@ -24,11 +25,41 @@ public class PasswordCracker {
             return;
         }
 
-        crackPassword();
+
+        // CRACK PASSWORD
+        int guessNum = 0;
+        String guess = "";
+        while (guess != password) {
+            System.out.println("\nguessnum - " + guessNum);
+
+            guess = GuessNumToString(guessNum);
+
+            System.out.println("guess: " + guess + "\n");
+            guessNum++;
+
+            if (guessNum > 500) return;
+        }
     }
 
-    private static void crackPassword() {
-        // INSERT CODE HERE
+    private static String GuessNumToString(int n) {
+        ArrayList<Character> chars = new ArrayList<Character>();
+        int basePerm = choices.length();
+
+        for (int i = 1; n >= 0; i++) {
+            int perm = (int) Math.pow((double) basePerm, (double) i);
+            int rem = n % basePerm;
+
+            System.out.println(n + " - " + perm + " - " + rem);
+
+            chars.add(choices.charAt(rem));
+            n -= perm;
+        }
+
+        // CONVERTING ARRAY LIST TO STRING AND RETURNING
+        char[] ca = new char[chars.size()];
+        for (int i = 0; i < chars.size(); i++) { ca[i] = chars.get(i); }
+        String s = new String(ca);
+        return s;
     }
 
     private static boolean validInput(String s) {
